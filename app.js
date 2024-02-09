86a83742a4f00ef901310edef9c550577d627b5e
GitMaster55 <144935449+SzkolnyGithub@users.noreply.github.com>
2024-02-09 14:01:13 +0100

    trzeba bedzie post zrobic

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
-
+app.use(express.json())
 function getData() {
     fetch('https://danepubliczne.imgw.pl/api/data/synop')
     .then(response => response.json())
     .then(jsonData => fs.writeFile('data.json', JSON.stringify(jsonData, null, 2), (err) => err && console.error(err)))
 }
-function getLocations() {
+/*function getLocations() { // to chyba bez sensu
     fetch('https://astronomia.zagan.pl/art/wspolrzedne.html')
     .then(response => response.set('charset=utf-8'))
     .then(utfresponse => utfresponse.text())
     .then(stringData => console.log(stringData))
     //.then(stringData => fs.writeFile("lokacje.txt", stringData, (err) => err && console.error(err)))
-}
-getLocations()
+}*/
+//getLocations()
 getData()
 setInterval(getData, 30000)
-const plik = require('./data.json')
 app.get('/dane', (req, res) => {
-    res.send(plik)
+    fs.readFile("./data.json", "utf8", (error, data) => {
+        if(error) {
+            console.log(error)
+            return;
+        }
+        res.send(JSON.parse(data))
+    })
 })
 app.get("/dane/id", (req, res) => {
     stacjaId = req.query.id
@@ -37,4 +42,4 @@ app.get("/dane/id", (req, res) => {
 
 app.listen(3000, () => {
     console.log(`Server is Listening on 3000`)
-})
+}) // https://www.geeksforgeeks.org/how-http-post-request-work-in-node-js/
