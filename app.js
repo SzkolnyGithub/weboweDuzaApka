2554287b6e125f800048bd04b5ebb2c002784d1f
GitMaster55 <144935449+SzkolnyGithub@users.noreply.github.com>
2024-03-01 10:10:23 +0100

    trzeba dodac lonLat do tabeli w bazie

--- app.js
+++ app.js
@@ -10,63 +10,65 @@ var con = mysql.createConnection({
   password : ''
 });
 
-
-
-sql = "CREATE DATABASE IF NOT EXISTS pogoda"
-con.query(sql, function (err, result, fields) {
-    if(err) throw err
-    console.log("baza danych utworzona pomyslnie")
-    con.query("ALTER DATABASE pogoda CHARACTER SET UTF8", function(err, result, fields) {
-        con.changeUser({database : 'pogoda'}, function(err) {
-            if (err) throw err;
-            sql1 = "CREATE TABLE IF NOT EXISTS dane(id_stacji VARCHAR(10) NOT NULL PRIMARY KEY, stacja VARCHAR(50) NOT NULL, data_pomiaru VARCHAR(10) NOT NULL, godzina_pomiaru VARCHAR(5) NOT NULL, temperatura VARCHAR(10) NOT NULL, predkosc_wiatru VARCHAR(5) NOT NULL, kierunek_wiatru VARCHAR(10) NOT NULL, wilgotnosc_wzgledna VARCHAR(10) NOT NULL, suma_opadu VARCHAR(5) NOT NULL, cisnienie VARCHAR(15) NULL)"
-            con.query(sql1, function (err, result, fields) {
-                if(err) throw err
-                console.log("tabela utworzona pomyslnie")
-                sql2 = "INSERT INTO dane VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-                con.query("DELETE FROM dane", function(err, result, fields) {
+function getData() {
+    sql = "CREATE DATABASE IF NOT EXISTS pogoda"
+    con.query(sql, function (err, result, fields) {
+        if(err) throw err
+        console.log("baza danych utworzona pomyslnie")
+        con.query("ALTER DATABASE pogoda CHARACTER SET UTF8", function(err, result, fields) {
+            con.changeUser({database : 'pogoda'}, function(err) {
+                if (err) throw err;
+                sql1 = "CREATE TABLE IF NOT EXISTS dane(id_stacji INT(10) NOT NULL PRIMARY KEY, stacja TEXT(50) NOT NULL, data_pomiaru DATE NOT NULL, godzina_pomiaru INT(5) NOT NULL, temperatura DOUBLE(10, 1) NOT NULL, predkosc_wiatru INT(5) NOT NULL, kierunek_wiatru INT(10) NOT NULL, wilgotnosc_wzgledna DOUBLE(10, 1) NOT NULL, suma_opadu DOUBLE(5, 1) NOT NULL, cisnienie DOUBLE(15, 1) NULL)"
+                con.query(sql1, function (err, result, fields) {
                     if(err) throw err
-                    fetch('https://danepubliczne.imgw.pl/api/data/synop')
-                    .then(response => response.json())
-                    .then(jsonData => {for(let i = 0; i < jsonData.length; i++) {
-                        con.query(sql2,[jsonData[i].id_stacji, jsonData[i].stacja, jsonData[i].data_pomiaru, jsonData[i].godzina_pomiaru, jsonData[i].temperatura, jsonData[i].predkosc_wiatru, jsonData[i].kierunek_wiatru, jsonData[i].wilgotnosc_wzgledna, jsonData[i].suma_opadu, jsonData[i].cisnienie], function(err, result, field) {
-                            if(err) throw err
-                            console.log("zapisano rzad: " + i)
-                        })
-                    }})
+                    console.log("tabela utworzona pomyslnie")
+                    sql2 = "INSERT INTO dane VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                    con.query("DELETE FROM dane", function(err, result, fields) {
+                        if(err) throw err
+                        fetch('https://danepubliczne.imgw.pl/api/data/synop')
+                        .then(response => response.json())
+                        .then(jsonData => {for(let i = 0; i < jsonData.length; i++) {
+                            con.query(sql2,[jsonData[i].id_stacji, jsonData[i].stacja, jsonData[i].data_pomiaru, jsonData[i].godzina_pomiaru, jsonData[i].temperatura, jsonData[i].predkosc_wiatru, jsonData[i].kierunek_wiatru, jsonData[i].wilgotnosc_wzgledna, jsonData[i].suma_opadu, jsonData[i].cisnienie], function(err, result, field) {
+                                if(err) throw err
+                                console.log("zapisano rzad: " + i)
+                            })
+                            let sql3 = "CREATE TABLE IF NOT EXISTS lokalizacje(miejscowosc TEXT(20) NOT NULL, lon DOUBLE(3, 10) NOT NULL, lat DOUBLE(3, 10) NOT NULL)"
+                            con.query(sql3, function(err, result, fields) {
+                                
+                            })
+                        }})
+                    })
                 })
             })
         })
     })
-})
-
-function getData() {
-    fetch('https://danepubliczne.imgw.pl/api/data/synop')
+    /*fetch('https://danepubliczne.imgw.pl/api/data/synop')
     .then(response => response.json())
-    .then(jsonData => fs.writeFile('data.json', JSON.stringify(jsonData, null, 2), (err) => err && console.error(err)))
+    .then(jsonData => fs.writeFile('data.json', JSON.stringify(jsonData, null, 2), (err) => err && console.error(err)))*/
 }
-/*function getLocations() { // to chyba bez sensu
-    fetch('https://astronomia.zagan.pl/art/wspolrzedne.html')
-    .then(response => response.set('charset=utf-8'))
-    .then(utfresponse => utfresponse.text())
-    .then(stringData => console.log(stringData))
-    //.then(stringData => fs.writeFile("lokacje.txt", stringData, (err) => err && console.error(err)))
-}*/
-//getLocations()
 getData()
 setInterval(getData, 3600000)
 app.get('/dane', (req, res) => {
-    fs.readFile("./data.json", "utf8", (error, data) => {
+    let sql = "SELECT * FROM dane"
+    con.query(sql, function(err, result) {
+        res.json(result)
+    })
+    /*fs.readFile("./data.json", "utf8", (error, data) => {
         if(error) {
             console.log(error)
             return;
         }
         res.send(JSON.parse(data))
-    })
+    })*/
 })
-app.get("/dane/id", (req, res) => {
-    stacjaId = req.query.id
-    fs.readFile("./data.json", "utf8", (error, data) => {
+app.get("/dane/:id", (req, res) => {
+    stacjaId = req.params['id']
+    let sql = "SELECT * FROM dane WHERE id_stacji = ?"
+    con.query(sql,[stacjaId], function(err, result) {
+        if(err) throw err
+        res.json(result)
+    })
+    /*fs.readFile("./data.json", "utf8", (error, data) => {
         if(error) {
             console.log(error)
             return;
@@ -75,10 +77,13 @@ app.get("/dane/id", (req, res) => {
         json = JSON.parse(data)
 
         res.send(json[stacjaId])
-    })
+    })*/
 })
-app.get('/', (req, res) => {
-     
+app.get('/latLon', (req, res) => {
+     fs.readFile("./filterLonLatCities.json", "utf8", (err, data) => {
+        if(err) throw err
+        res.json(JSON.parse(data))
+     })
 })
 
 app.listen(3000, () => {
